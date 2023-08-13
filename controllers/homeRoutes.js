b126b8024b0fee0b4e673059bdcb648878ccfa11
Ron Buttermore <ron.buttermore@gmail.com>
2023-08-13 07:50:19 -0600

    added route for viewing posts

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -23,6 +23,28 @@ router.get('/', async (req, res) => {
     }
   });
 
+  router.get('/post/:id', async (req, res) => {
+    try {
+      const postData = await Post.findByPk(req.params.id, {
+        include: [
+          {
+            model: User,
+            attributes: ['name'],
+          },
+        ],
+      });
+  
+      const post = postData.get({ plain: true });
+  
+      res.render('post', {
+        ...post,
+        logged_in: req.session.logged_in
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
 router.get('/dashboard', async (req, res) => {
     res.render('dashboard');
 });
---
+++ public/js/post.js
@@ -0,0 +1,5 @@
+const post = () => {
+    document.location.replace('/post');
+}
+
+document.querySelector('.post').addEventListener('click', post);
