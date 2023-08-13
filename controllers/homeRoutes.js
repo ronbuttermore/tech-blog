df0e245d972ae23e7ecdc301023d3c14bbc158fd
Ron Buttermore <ron.buttermore@gmail.com>
2023-08-13 04:18:21 -0600

    added existing posts to homepage

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,8 +1,26 @@
 const router = require('express').Router();
+const { Post, User } = require('../models');
 
 router.get('/', async (req, res) => {
-    res.render('homepage');
-});
+    try {
+      const postData = await Post.findAll({
+        include: [
+            {
+              model: User,
+              attributes: ['name'],
+            },
+        ],
+    });
+  
+      const posts = postData.map((post) => post.get({ plain: true }));
+  
+      res.render('homepage', { 
+        posts, 
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
 router.get('/dashboard', async (req, res) => {
     res.render('dashboard');
