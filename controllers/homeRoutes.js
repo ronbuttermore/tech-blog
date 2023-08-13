765d9a1f4659fc38d9524853f336834ef4ceb972
Ron Buttermore <ron.buttermore@gmail.com>
2023-08-13 06:48:20 -0600

    added login and logout functionality

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -15,7 +15,8 @@ router.get('/', async (req, res) => {
       const posts = postData.map((post) => post.get({ plain: true }));
   
       res.render('homepage', { 
-        posts, 
+        posts,
+        logged_in: req.session.logged_in
       });
     } catch (err) {
       res.status(500).json(err);
