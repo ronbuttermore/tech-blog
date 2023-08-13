34e802ca05149daf21497d31addc96df1211d494
Ron Buttermore <ron.buttermore@gmail.com>
2023-08-13 04:07:33 -0600

    added models and seeds for blogpost_db

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,4 +4,12 @@ router.get('/', async (req, res) => {
     res.render('homepage');
 });
 
+router.get('/dashboard', async (req, res) => {
+    res.render('dashboard');
+});
+
+router.get('/login', async (req, res) => {
+    res.render('login');
+});
+
 module.exports = router;
